3872fc5d1a3f935ce2d2d96ecd17c083d79a073c
roni-git <roni.gilboa10@gmail.com>
2022-04-13 18:21:15 +0300

    roni

---
+++ chat.js
@@ -0,0 +1,8 @@
+var users=[];//contain the chats of all the users
+var userChats=[];
+var massages=[]; //structs of name and the massage he sended and the data and the image.
+/*
+שלבי עבודה:
+ליצור בגאווה סקריפט מערך, 
+ואז לקחת את החלקים המוכנים מהקוד, 
+ופשוט לכתוב שם את הגישה דרך המערך במקום את השמות עצמם.
